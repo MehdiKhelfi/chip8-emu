b2ed70895b3632bcf93cb85a8e652498d65d0493
MochaLatte <43214538+MehdiKhelfi@users.noreply.github.com>
2020-10-28 21:26:03 +0100

    Accidentally uploaded this don't mind it

--- src/Testsdl.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
-#include <windows.h> 
-int main(void) 
-{ 
-    Beep(1000, 300); 
-    printf("\n \n \t This is a dummy program for Beep."); 
-    getchar(); 
-    return 0; 
-} 
