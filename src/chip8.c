532bc0735b9f10848c69c4ae5b7ef189398db6b1
MochaLatte <43214538+MehdiKhelfi@users.noreply.github.com>
2020-10-29 20:39:48 +0100

    Commented the code

--- src/chip8.c
+++ src/chip8.c
@@ -5,19 +5,19 @@
 #include <time.h>
 #include <string.h>
 typedef struct CHIP8{
-    uint16_t stack[16];
+    uint16_t stack[16];// The chip 8 can store 16 addresses (for calls)
     uint8_t memory[4096]; // chip 8 has 4kb of mem
-    uint8_t V[0xF];
-    uint16_t I;
-    uint16_t pc;
-    uint16_t opcode;
-    int sp;
-    uint8_t graphics[64][32];
-    uint8_t key[16];
+    uint8_t V[0xF]; //14 basic 8bit registers and one "Flag" register (for borrow, carry, collisions ...)
+    uint16_t I; // I reg to store 12bit addresses
+    uint16_t pc; // Program counter, indicates which instruction should be executed
+    uint16_t opcode; // Well... it's the opcode
+    int sp; // Stack pointer, points to the top of the stack
+    uint8_t graphics[64][32]; //2d array for graphics (64 * 32 = 2048 of vram / resolution)
+    uint8_t key[16]; //pressed keys
     uint8_t delay_t; //delay timer
     uint8_t sound_t; // sound timer
-    bool drawFlag;
-    bool keyPressed;
+    bool drawFlag; //If it's true, the program draw on screen
+    bool keyPressed; //The name is quite explicit...
 } CHIP8;
 
 unsigned char font[80] = 
@@ -41,6 +41,7 @@ unsigned char font[80] =
 };
 void init(CHIP8 *c)
 {
+    //Filling all arrays with zeroes
     memset(c->graphics, 0, (64*32)*sizeof(uint8_t));
     memset(c->memory, 0, 4096*sizeof(uint8_t));
     memset(c->stack, 0, 16*sizeof(uint8_t));
@@ -49,10 +50,10 @@ void init(CHIP8 *c)
     for(int i = 0; i < 80; i++){
         c->memory[i] = font[i]; //load font in mem
     }
-    c->pc = 0x200;
-    c->sp = -1;
-    c->I = c->opcode = c->keyPressed = c->drawFlag = c->delay_t = c->sound_t = 0;
-    srandom(time(NULL));
+    c->pc = 0x200; //chip 8 programs start at 0x200 in mem
+    c->sp = -1; //as the stack is empty, I initialize the stack pointer at -1
+    c->I = c->opcode = c->keyPressed = c->drawFlag = c->delay_t = c->sound_t = 0; // set everything to zero
+    srandom(time(NULL)); //seed the random function
 }
 
 int loadfile(CHIP8 *c, const char *argv)
@@ -66,30 +67,32 @@ int loadfile(CHIP8 *c, const char *argv)
         return 1;
     }
     rewind(f);
-    fread(c->memory+0x200, size, 1, f);
+    fread(c->memory+0x200, size, 1, f); // loads the program at 0x200 in mem
     fclose(f);
 }
 
 void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
 {
-    c->opcode = c->memory[c->pc] << 8 | c->memory[c->pc + 1];
-    uint8_t x = (c->opcode & 0x0F00) >> 8;
-    uint8_t y = (c->opcode & 0x00F0) >> 4;
-    uint8_t n = (c->opcode & 0x000F);
-    uint16_t nnn = (c->opcode & 0x0FFF);
-    uint8_t kk = c->opcode & 0x00FF;
-    int num = c->V[x];
+    c->opcode = c->memory[c->pc] << 8 | c->memory[c->pc + 1]; // merge the bytes into a single, two bytes opcode
+    uint8_t x = (c->opcode & 0x0F00) >> 8; // x register in opcodes
+    uint8_t y = (c->opcode & 0x00F0) >> 4; // y register in opcodes
+    uint8_t n = (c->opcode & 0x000F); // the nibble in opcodes
+    uint16_t nnn = (c->opcode & 0x0FFF); // 12 bits values in opcodes
+    uint8_t kk = c->opcode & 0x00FF; // bytes in opcode
+    int num = c->V[x]; // variable used for BCD (see opcode FX33)
     switch (c->opcode & 0xF000)
     {
         case 0x0000:
             switch (c->opcode & 0x000F)
             {
             case 0x0000:
+                // CLS, simply clears the screen (so the graphics buffer here)
                 memset(c->graphics, 0, (64*32)*sizeof(uint8_t));
                 c->drawFlag = true;
                 c->pc += 2;
                 break;
             case 0x000E:
+                // RET, pops address from stack and loads it into pc
                 c->pc = c->stack[c->sp];
                 c->sp--;
                 c->pc += 2;
@@ -102,35 +105,42 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             break;
 
         case 0x1000:
+            //JP NNN, jumps to the address NNN
             c->pc = nnn;
             break;
 
         case 0x2000:
+            //CALL NNN, pushes pc in the stack and jumps to the subroutine at NNN
             c->sp++;
             c->stack[c->sp] = c->pc;
             c->pc = nnn;
             break;
         case 0x3000:
+            //Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
             if(c->V[x] == kk)
                 c->pc += 2;
             c->pc+=2;
             break;
         case 0x4000:
+            //Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a jump to skip a code block)
             if(c->V[x] != kk)
                 c->pc += 4;
             else
                 c->pc += 2;
             break;
         case 0x5000:
+            //Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block)
             if(c->V[x] == c->V[y])
                 c->pc += 2;
             c->pc += 2;
             break;
         case 0x6000:
+            //Sets VX to NN.
             c->V[x] = kk;
             c->pc += 2;
             break;
         case 0x7000:
+            //Adds NN to VX. (Carry flag is not changed)
             c->V[x] += kk;
             c->pc += 2;
             break;
@@ -138,42 +148,51 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             switch (c->opcode & 0x000F)
             {
                 case 0x0:
+                    //Sets VX to the value of VY.
                     c->V[x] = c->V[y];
                     c->pc += 2;
                     break;
                 case 0x1:
+                    //Sets VX to VX OR VY. (Bitwise OR operation)
                     c->V[x] |= c->V[y];
                     c->pc += 2;
                     break;
                 case 0x2:
+                    //Sets VX to VX and VY. (Bitwise AND operation)
                     c->V[x] &= c->V[y];
                     c->pc += 2;
                     break;
                 case 0x3:
+                    //Sets VX to VX xor VY.
                     c->V[x] ^= c->V[y];
                     c->pc += 2;
                     break;
                 case 0x4:
+                    //Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
                     c->V[0xF] = ((int)c->V[x] + (int)c->V[y]) > 255 ? 1 : 0;
                     c->V[x] += c->V[y];
                     c->pc += 2;
                     break;
                 case 0x5:
+                    //VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                     c->V[0xF] = (c->V[x] > c->V[y]) ? 1 : 0;
                     c->V[x] -= c->V[y];
                     c->pc += 2;
                     break;
                 case 0x6:
+                    //Stores the least significant bit of VX in VF and then shifts VX to the right by 1
                     c->V[0xF] = c->V[x] & 1;
                     c->V[x] >>= 1;
                     c->pc += 2;
                     break;
                 case 0x7:
+                    //Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                     c->V[0xF] = c->V[y] > c->V[x] ? 1 : 0;
                     c->V[x] = c->V[y] - c->V[x];
                     c->pc += 2;
                     break;
                 case 0xE:
+                    //Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
                     c->V[0xF] = (c->V[x] & 0x80) >> 7; // 0x80 = 0b10000000
                     c->V[x] <<= 1;
                     c->pc += 2;
@@ -185,19 +204,23 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             }
             break;
         case 0x9000:
+            // Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)
             if(c->V[x] != c->V[y]){
                 c->pc += 2;
             }
             c->pc +=2;
             break;
         case 0xA000:
+            // Sets I to the address NNN.
             c->I = nnn;
             c->pc += 2;
             break;
         case 0xB000:
+            // Jumps to the address NNN plus V0.
             c->pc = nnn + c->V[0];
             break;
-        case 0xC000:{
+        case 0xC000:{ 
+            // Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
             uint8_t rand = random();
             c->V[x] = rand & kk;
             c->pc += 2;
@@ -205,15 +228,18 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
         }
         case 0xD000:
         {
+            //Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. 
+            //Each row of 8 pixels is read as bit-coded starting from memory location I
+            //I value doesn’t change after the execution of this instruction.
             uint8_t row;
             c->V[0xF] = 0;
-            for(int i = 0; i != n; i++){
+            for(int i = 0; i != n; i++){ //loop through sprite lines
                 row = c->memory[c->I+i];
-                for(int a = 0; a != 8; a++){
-                    int pixel = row & (0x80 >> a);
+                for(int a = 0; a != 8; a++){ //loop through bits of rows in the sprites
+                    int pixel = row & (0x80 >> a); // Check if the bit is 1 or 0
                     if(pixel && c->V[y]+i <= 64*2 && c->V[x]+a <= 32*2){
-                        c->V[0xF] = (c->graphics[c->V[x]+a][c->V[y]+i] > 0) ? 1 : 0;
-                        c->graphics[c->V[x]+a][c->V[y]+i] ^= 1;
+                        c->V[0xF] = (c->graphics[c->V[x]+a][c->V[y]+i] > 0) ? 1 : 0; //set VF if a bit is flipped from set to unset (collision basically)
+                        c->graphics[c->V[x]+a][c->V[y]+i] ^= 1; // xor the sprite bit with the graphic buffer bit
                         c->drawFlag = true;
                     }
                     
@@ -225,12 +251,14 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
         case 0xE000:
             switch(c->opcode & 0x000F){
                 case 0xE:
+                    //Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)
                     if(c->key[c->V[x]]){
                         c->pc += 2;
                     }
                     c->pc += 2;
                     break;
-                case 0x1:
+                case 0x1: 
+                    //Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
                     if(c->key[c->V[x]] == 0){
                         c->pc += 2;
                     }
@@ -245,10 +273,12 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     switch (c->opcode & 0x000F)
                     {
                         case 0x7:
+                            // Sets VX to the value of the delay timer.
                             c->V[x] = c->delay_t;
                             c->pc += 2;
                             break;
                         case 0xA:
+                            //A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
                             c->keyPressed = false;
                             for(int i = 0; i != 16; i++){
                                 if(c->key[i]){
@@ -257,7 +287,6 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                                 }
                                 else if(!c->keyPressed)
                                     return;
-                            
                             }
                             c->pc += 2;
                             break;
@@ -271,14 +300,17 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     switch (c->opcode & 0x000F)
                     {
                     case 0x5:
+                        // Sets the delay timer to VX.
                         c->delay_t = c->V[x];
                         c->pc += 2;
                         break;
                     case 0x8:
+                        // Sets the sound timer to VX.
                         c->sound_t = c->V[x];
                         c->pc += 2; 
                         break;
                     case 0xE:
+                        // Adds VX to I. VF is not affected
                         c->I += c->V[x];
                         c->V[0xF] = c->I > 0x0FFF ? 1 : 0;
                         c->pc += 2;
@@ -290,10 +322,14 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     }
                    break;
                 case 2:
+                    // Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
                     c->I = c->V[x] * 5;
                     c->pc += 2;
                     break;
                 case 3:
+                    // Stores the binary-coded decimal representation of VX
+                    //with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2.
+                    //(In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
                     for(int i = 2; i != -1; i--){
                         c->memory[c->I+i] = num%10;
                         num /= 10;
@@ -301,12 +337,15 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     c->pc += 2;
                     break;
                 case 5:
+                    //Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
                     for(int i = 0; i <= x; i++){
                         c->memory[c->I+i] = c->V[i];
                     }
                     c->pc += 2;
                     break;
                 case 6:
+                    //Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written
+                    //I is then set to I + X + 1
                     for(int i = 0; i <= x; i++){
                         c->V[i] = c->memory[c->I+i];
                     }
@@ -325,5 +364,5 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             break;
     }
     if(c->delay_t)
-        c->delay_t--;
+        c->delay_t--; //decrement delay timer
 }
--- src/main.c
+++ src/main.c
@@ -4,8 +4,6 @@
 #include <stdint.h>
 #include <SDL2/SDL.h>
 #include "chip8.c"
-#define W_HEIGHT 32
-#define W_WIDTH 64
 int main(int argc, char **argv)
 {
     uint8_t SDL_keys[16] ={
@@ -25,22 +23,23 @@ int main(int argc, char **argv)
     SDLK_g,
     SDLK_h,
     SDLK_v,
-    };
+    }; //defines all keys
+    //Create a chip8 struct and struct pointer to use it in functions
     CHIP8 *c, chip8;
     c = &chip8;
-    init(c);
+    init(c); // initialize our chip8 struct 
     if(argc < 2){
         printf("Usage : chip8 <path to file>");
         return 1;
     }
-    loadfile(c, argv[1]);
+    loadfile(c, argv[1]); //load the rom gave as an argument in mem
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0 )
     {
         fprintf(stdout,"Failed to initialize SDL... Error : %s\n",SDL_GetError());
         return -1;
     }
     SDL_Window* window = NULL;
-    window = SDL_CreateWindow("Chip8 emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 128*2, 64*2, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Chip8 emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 64*4, 32*4, SDL_WINDOW_SHOWN); //create the window with 64*4 by 32*4 res
     if (window == NULL){
         printf( "An error occured while creating the window... SDL_Error: %s\n", SDL_GetError());
         exit(2);
@@ -52,22 +51,21 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
     }
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    printf("Done");
-    SDL_Rect rect;
+    SDL_RenderClear(renderer); //fill the window in black
+    SDL_Rect rect; //declare rect for drawing
     while(true)
     {
         emulatecycle(c, renderer);
         if(chip8.drawFlag){
             for(int i = 0; i != 64; i++){
-                for(int a = 0; a != 32; a++){
+                for(int a = 0; a != 32; a++){ //loop through the graphics buffer to draw pixel
                     if(chip8.graphics[i][a]){
                         rect.x = i*4;
-                        rect.y = a*4;
-                        rect.w = rect.h = 4;
+                        rect.y = a*4; //set the coordinates of the rectangle
+                        rect.w = rect.h = 4; //set the size of the rectangle to 4 which is the scaling factor used in this program (the window is 64 * 4 and 32 * 4)
                         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
                         SDL_RenderDrawRect(renderer, &rect);
-                        SDL_RenderFillRect(renderer, &rect);
+                        SDL_RenderFillRect(renderer, &rect); // draw white rects
                     }
                     else{
                         rect.x = i*4;
@@ -75,15 +73,15 @@ int main(int argc, char **argv)
                         rect.w = rect.h = 4;
                         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
                         SDL_RenderDrawRect(renderer, &rect);
-                        SDL_RenderFillRect(renderer, &rect);
+                        SDL_RenderFillRect(renderer, &rect); //draw black rects
                     }
                 }
             }
-            SDL_RenderPresent(renderer);
-            chip8.drawFlag = false;
+            SDL_RenderPresent(renderer); //render the drawings to screen
+            chip8.drawFlag = false;// set the drawflag to false back
         }
         SDL_Event event;
-        while(SDL_PollEvent(&event)){
+        while(SDL_PollEvent(&event)){ // Check sdl events (keys)
             if(event.type == SDL_QUIT)
                 exit(0);
             else if(event.type == SDL_KEYDOWN){
@@ -93,12 +91,10 @@ int main(int argc, char **argv)
                             SDL_Delay(100);
                             exit(0);
                             break;
-                        default:
-                            break;
                 }
                 for (int i = 0; i < 16; i++){
                     if (event.key.keysym.sym == SDL_keys[i]){
-                        chip8.key[i] = 1;
+                        chip8.key[i] = 1;//set pressed keys to one
                     }
                 }    
                 
@@ -106,7 +102,7 @@ int main(int argc, char **argv)
             else if(event.type == SDL_KEYUP){
                 for (int i = 0; i < 16; i++){
                     if (event.key.keysym.sym == SDL_keys[i]){
-                        chip8.key[i] = 0;
+                        chip8.key[i] = 0; //set released keys to zero
                     }
                 }
             }
