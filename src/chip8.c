4e57c82633873bc035dfbf911a6117bb4f701b08
MochaLatte <43214538+MehdiKhelfi@users.noreply.github.com>
2020-10-29 10:48:02 +0100

    Commented out prints and added exits

--- src/chip8.c
+++ src/chip8.c
@@ -79,68 +79,69 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
     uint16_t nnn = (c->opcode & 0x0FFF);
     uint8_t kk = c->opcode & 0x00FF;
     int num = c->V[x];
-    printf("pc %04x opcode %04x Asm : ", c->pc, c->opcode);
+    //printf("pc %04x opcode %04x Asm : ", c->pc, c->opcode);
     switch (c->opcode & 0xF000)
     {
         case 0x0000:
             switch (c->opcode & 0x000F)
             {
             case 0x0000:
-                printf("CLS");
+                //printf("CLS");
                 memset(c->graphics, 0, (64*32)*sizeof(uint8_t));
                 c->drawFlag = true;
                 c->pc += 2;
                 break;
             case 0x000E:
-                printf("RET");
+                //printf("RET");
                 c->pc = c->stack[c->sp];
                 c->sp--;
                 c->pc += 2;
                 break;
             default:
                 printf("Unknown opcode %#X", c->opcode);
+                exit(1)
                 break;
             }
             break;
 
         case 0x1000:
-            printf("JP %#X",  nnn);
+            //printf("JP %#X",  nnn);
             c->pc = nnn;
             break;
 
         case 0x2000:
-            printf("CALL %#X", nnn);
+            //printf("CALL %#X", nnn);
             c->sp++;
             c->stack[c->sp] = c->pc;
             c->pc = nnn;
             break;
         case 0x3000:
-            printf("SE V%x, %#X",x, kk);
+            //printf("SE V%x, %#X",x, kk);
             if(c->V[x] == kk)
                 c->pc += 2;
             c->pc+=2;
             break;
         case 0x4000:
-            printf("SNE V%x, %#X",x, kk);
+            //printf("SNE V%x, %#X",x, kk);
             if(c->V[x] != kk)
                 c->pc += 4;
             else
                 c->pc += 2;
             break;
         case 0x5000:
-            printf("SE V%x, V%x", x, y);
+            //printf("SE V%x, V%x", x, y);
             if(x == y)
                 c->pc += 4;
             else
                 c->pc += 2;
             break;
         case 0x6000:
-            printf("LD V%x, %#X", x, kk);
+            //printf("LD V%x, %#X", x, kk);
             c->V[x] = kk;
             c->pc += 2;
             break;
         case 0x7000:
-            printf("ADD V%x, %#X", x, kk);
+            //printf("ADD V%x, %#X", x, kk);
             c->V[x] += kk;
             c->pc += 2;
             break;
@@ -148,79 +149,80 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             switch (c->opcode & 0x000F)
             {
             case 0x0:
-                printf("LD V%x, V%x", x, y);
+                //printf("LD V%x, V%x", x, y);
                 c->V[x] = c->V[y];
                 c->pc += 2;
                 break;
             case 0x1:
-                printf("OR V%x, V%x", x, y);
+                //printf("OR V%x, V%x", x, y);
                 c->V[x] |= c->V[y];
                 c->pc += 2;
                 break;
             case 0x2:
-                printf("AND V%x, V%x", x, y);
+                //printf("AND V%x, V%x", x, y);
                 c->V[x] &= c->V[y];
                 c->pc += 2;
                 break;
             case 0x3:
-                printf("XOR V%x, V%x", x, y);
+                //printf("XOR V%x, V%x", x, y);
                 c->V[x] ^= c->V[y];
                 c->pc += 2;
                 break;
             case 0x4:
-                printf("ADD V%x, V%x", x, y);
+                //printf("ADD V%x, V%x", x, y);
                 c->V[0xF] = ((int)c->V[x] + (int)c->V[y]) > 255 ? 1 : 0;
                 c->V[x] += c->V[y];
                 c->pc += 2;
                 break;
             case 0x5:
-                printf("SUB V%x, V%x", x, y);
+                //printf("SUB V%x, V%x", x, y);
                 c->V[0xF] = (c->V[x] > c->V[y]) ? 1 : 0;
                 c->V[x] -= c->V[y];
                 c->pc += 2;
                 break;
             case 0x6:
-                printf("SHR V%x 1", x);
+               //printf("SHR V%x 1", x);
                 c->V[0xF] = c->V[x] & 1;
                 c->V[x] >>= 1;
                 c->pc += 2;
                 break;
             case 0x7:
-                printf("SUBN V%x, V%x", x, y);
+                //printf("SUBN V%x, V%x", x, y);
                 c->V[x] -= c->V[y];
                 c->V[0xF] = c->V[y] > c->V[x] ? 1 : 0;
                 c->pc += 2;
                 break;
             case 0xE:
-                printf("SHL V%x 1", x);
+                //printf("SHL V%x 1", x);
                 c->V[0xF] = c->V[x] & 0x80; // 0x80 = 0b10000000
                 c->V[x] <<= 1;
                 c->pc += 2;
                 break;
             default:
                 printf("Unknown opcode %#X", c->opcode);
+                exit(1);
                 break;
             }
             break;
         case 0x9000:
-            printf("SNE V%x, V%x", x, y);
+            //printf("SNE V%x, V%x", x, y);
             if(c->V[x] != c->V[y]){
                 c->pc += 2;
             }
             c->pc +=2;
             break;
         case 0xA000:
-            printf("LD I, %#X", nnn);
+            //printf("LD I, %#X", nnn);
             c->I = nnn;
             c->pc += 2;
             break;
         case 0xB000:
-            printf("JP V0 + %#x", nnn);
+            //printf("JP V0 + %#x", nnn);
             c->pc = nnn + c->V[0];
             c->pc += 2;
             break;
         case 0xC000:
-            printf("LD V%x, (RND AND %#x)", x, kk);
+            //printf("LD V%x, (RND AND %#x)", x, kk);
             srand(time(NULL));
             uint8_t random = rand() % 256;
             c->V[x] = random & kk;
@@ -228,7 +230,7 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
             break;
         case 0xD000:
         {
-            printf("DRW V%x = %x, V%x = %x, %#x", x, c->V[x], y, c->V[y], n);
+            //printf("DRW V%x = %x, V%x = %x, %#x", x, c->V[x], y, c->V[y], n);
             uint8_t row;
             c->V[0xF] = 0;
             for(int i = 0; i != n; i++){
@@ -249,14 +251,14 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
         case 0xE000:
             switch(c->opcode & 0x000F){
                 case 0xE:
-                    printf("SKP if key = V%x (%x) pressed", x, c->V[x]);
+                    //printf("SKP if key = V%x (%x) pressed", x, c->V[x]);
                     if(c->key[c->V[x]]){
                         c->pc += 2;
                     }
                     c->pc += 2;
                     break;
                 case 0x1:
-                    printf("SKP if key V%x (%x) not pressed", x, c->V[x]);
+                    //printf("SKP if key V%x (%x) not pressed", x, c->V[x]);
                     if(c->key[c->V[x]] == 0){
                         c->pc += 2;
                     }
@@ -271,12 +273,12 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     switch (c->opcode & 0x000F)
                     {
                         case 0x7:
-                            printf("LD V%x, DT");
+                            //printf("LD V%x, DT");
                             c->V[x] = c->delay_t;
                             c->pc += 2;
                             break;
                         case 0xA:
-                            printf("LD V%x, K\n", x);
+                            //printf("LD V%x, K\n", x);
                             c->keyPressed = false;
                             for(int i = 0; i != 16; i++){
                                 if(c->key[i]){
@@ -291,6 +293,7 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                             break;
                         default:
                             printf("Unknown opcode %#X", c->opcode);
+                            exit(1);
                             break;
                     }
                     break;
@@ -298,23 +301,24 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     switch (c->opcode & 0x000F)
                     {
                     case 0x5:
-                        printf("LD DT, V%x", x);
+                        //printf("LD DT, V%x", x);
                         c->delay_t = c->V[x];
                         c->pc += 2;
                         break;
                     case 0x8:
-                        printf("LD ST, V%x", x);
+                        //printf("LD ST, V%x", x);
                         c->sound_t = c->V[x];
                         c->pc += 2; 
                         break;
                     case 0xE:
-                        printf("ADD I, V%x", x);
+                        //printf("ADD I, V%x", x);
                         c->I += c->V[x];
                         c->V[0xF] = c->I > 0x0FFF ? 1 : 0;
                         c->pc += 2;
                         break;
                     default:
                         printf("Unknown opcode %#X", c->opcode);
+                        exit(1);
                         break;
                     }
                    break;
@@ -323,11 +327,11 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     c->pc += 2;
                     break;
                 case 3:
-                    printf("BASE NUM : %d\n", num);
+                    //printf("BASE NUM : %d\n", num);
                     for(int i = 2; i != -1; i--){
                         c->memory[c->I+i] = num%10;
-                        printf("V%x = %d, SetNum = %d, i = %d\n", x, c->V[x], num%10, i);
-                        printf("Memory at I+%d: %x\n", i, c->memory[c->I+i]);
+                        //printf("V%x = %d, SetNum = %d, i = %d\n", x, c->V[x], num%10, i);
+                        //printf("Memory at I+%d: %x\n", i, c->memory[c->I+i]);
                         num /= 10;
                     }
                     c->pc += 2;
@@ -346,14 +350,16 @@ void emulatecycle(CHIP8 *c, SDL_Renderer* renderer)
                     break;
                 default:
                     printf("Unknown opcode %#X", c->opcode);
+                    exit(1);
                     break;
             }
             break;
         default:
             printf("Unknown opcode %#X", c->opcode);
+            exit(1);
             break;
     }
-    printf("\n");
+    //printf("\n");
     if(c->delay_t)
         c->delay_t--;
     /*if(c->sound_t)
